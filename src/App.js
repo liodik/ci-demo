ada2625014c67789c1ded6b549e4e35a169203d1
Volodymyr Meduna <volodymyr.meduna@perfectial.com>
2022-05-22 10:32:17 +0300

    new

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>new branch</p>
+        <p>netlify no deploy. Change branche</p>
         <a
           className="App-link"
           href="https://reactjs.org"
